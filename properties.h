9642e3625d97de5aca850943ed33fffb3b61f180
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2024-08-02 18:08:01 +0200

    Add properties.h

---
+++ properties.h
@@ -0,0 +1,6 @@
+#ifndef __PROPERTIES_H__
+#define __PROPERTIES_H__
+
+void properties_load(FILE *);
+
+#endif /* __PROPERTIES_H__ */
--- test/main.c
+++ test/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void properties_load(FILE *file);
+#include "../properties.h"
 
 int
 main(int argc, char **argv)
